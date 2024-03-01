ff91c396b1245a72b4ae04d2817ba5d951efe3a1
Kirill <opoxmeloid@yandex.ru>
2024-03-01 09:25:08 +0200

    Ready, set, go!

---
+++ README.md
@@ -0,0 +1,92 @@
+# MeBus
+
+MeBus is a type safe, runtime validated, event bus. It is a wrapper around native browser event system, and it uses Zod. 
+
+## Use cases
+
+- Micro frontends communication
+
+  Microfrontends are, by design, disconnected from each other. They are independent applications that are composed together to form a single user interface. This means that they need a way to communicate with each other. MeBus can be used to create a type safe a communication layer between microfrontends.
+  Microfronteds only need to share same event schema.
+
+- Event driven architecture
+
+  MeBus can be used to create an event driven architecture. It can be used to create a communication layer between different parts of an application.
+
+## Installation
+
+You can install Mebus using npm:
+
+```bash
+npm i mebus
+```
+
+## Usage
+
+```javascript
+import { MeBus } from 'mebus';
+import z from "zod";
+
+const MyEventSchema = {
+  event1: z.object({
+    payload: z.string(),
+  }),
+  event2: z.object({
+    id: z.number(),
+  }),
+  ["namespace::event3"]: z.object({
+    foo: z.boolean(),
+  }),
+  // add more events as needed
+};
+
+const bus = new MeBus(MyEventSchema);
+
+const handler = (payload) => {
+  console.log("event1 received", payload);
+};
+
+bus.subscribe("event1", handler);
+
+bus.publish("event1", { payload: "Hello, world!" });
+
+bus.unsubscribe("event1", handler);
+```
+
+## API
+
+* `constructor(eventSchema: Record<string, z.Schema)`
+  Creates a new instance of MeBus.
+
+    - `eventSchema`: An object that defines the schema for the events. The keys are the event names and the values are the Zod schemas for the event payloads.
+
+* `subscribe(eventName: string, handler: (payload: any) => void): void`
+
+  Subscribes to an event. The `handler` function will be called whenever the event is published.
+
+    - `eventName`: The name of the event to subscribe to.
+
+    - `handler`: The function to call when the event is published. The function will be called with the payload of the event.
+
+* `unsubscribe(eventName: string, handler: (payload: any) => void): void`
+
+  Unsubscribes from an event.
+
+    - `eventName`: The name of the event to unsubscribe from.
+
+    - `handler`: The function to unsubscribe.
+
+* `publish(eventName: string, payload: any): void`
+
+    Publishes an event with window.dispatchEvent
+
+    - `eventName`: The name of the event to publish.
+
+    - `payload`: The payload of the event.
+
+## License
+
+MIT
+
+
+
---
+++ index.ts
@@ -0,0 +1,90 @@
+import z from "zod";
+
+export type EventSchema = Record<string, z.Schema>;
+
+const isCustomEvent = (e: Event): e is CustomEvent => {
+  return e instanceof CustomEvent;
+};
+
+/**
+ * MeBus is a typed and end-to-end runtime validated message bus for the browser.
+ * @param {EventSchema} eventSchema - Schema with definitions for all the event types and their payloads.
+ *
+ * @example
+ * import z from "zod";
+ *
+ * const MyEventSchema = {
+ *  event1: z.object({  payload: z.string(), }),
+ *  event2: z.object({  id: z.number(), }),
+ * };
+ *
+ * const bus = new MessageBus(MyEventSchema);
+ *
+ * bus.subscribe("event1", (payload) => console.log(payload));
+ * bus.publish("event2", { id: 1 });
+ */
+export class MeBus<T extends EventSchema, K extends keyof T & string> {
+  private eventSchema: T;
+
+  constructor(eventSchema: T) {
+    if (typeof window === "undefined") {
+      throw new Error("[MeBus] MeBus is only available in the browser");
+    }
+    this.eventSchema = eventSchema;
+  }
+
+  /**
+   * Subscribes to a specific event type and registers a listener function to be called when the event is triggered.
+   * @param type - The event type to subscribe to.
+   * @param listener - The listener function to be called when the event is triggered. It receives the payload of the event as a parameter.
+   * @returns A function that can be called to unsubscribe from the event.
+   */
+  public subscribe(type: K, listener: (payload: z.infer<T[K]>) => void) {
+    const schema = this.eventSchema[type];
+    if (!schema) {
+      throw new Error(`[MeBus] No schema found for event: ${type}`);
+    }
+
+    const abortController = new AbortController();
+
+    window.addEventListener(
+      type,
+      (e) => {
+        if (!isCustomEvent(e)) return;
+        const payload = e.detail;
+        const validation = schema.safeParse(payload);
+        if (!validation.success) {
+          throw new Error(
+            validation.error.errors.map((e) => e.message).join("\n")
+          );
+        }
+        listener(validation.data);
+      },
+      { signal: abortController.signal }
+    );
+
+    return () => abortController.abort();
+  }
+
+  /**
+   * Publishes a message of a specific type with the given payload.
+   *
+   * @param type - The type of the message.
+   * @param payload - The payload of the message.
+   * @returns void
+   * @throws Error if no schema is found for the event or if the payload fails validation.
+   */
+  public publish(type: K, payload: z.infer<T[K]>): void {
+    const schema = this.eventSchema[type];
+    if (!schema) {
+      throw new Error(`[MeBus] No schema found for event: ${type}`);
+    }
+    const validation = schema.safeParse(payload);
+    if (!validation.success) {
+      throw new Error(validation.error.errors.map((e) => e.message).join("\n"));
+    }
+    window.dispatchEvent(new CustomEvent(type, { detail: payload }));
+  }
+}
+
+export default MeBus;
